ad05ac970da1880d9defc60db04e41f7860b9c85
agit <agitagustian@gmail.com>
2023-02-25 12:16:03 +0700

    edit halaman admin

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,17 +1,20 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Admin extends CI_Controller {
+class Admin extends CI_Controller
+{
     function __construct()
     {
         parent::__construct();
-        $this->load->model('M_app'); 
+        $this->load->model('M_app');
         $this->load->helper('cookie');
-    } 
+    }
     public function index()
-    {  
-        if(get_cookie('level') != "Admin") redirect("login", 'refresh'); 
+    {
+        //if(get_cookie('level') != "Admin") redirect("login", 'refresh'); 
         $data = null;
-        $this->load->view('admin/menu',$data); 
+        $this->load->view('template/header', $data);
+        $this->load->view('admin/menu', $data);
+        $this->load->view('template/footer', $data);
     }
 }
