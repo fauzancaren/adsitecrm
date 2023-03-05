ad012222302de901f61d7781a07ef3e922eed4cd
agit <agitagustian@gmail.com>
2023-03-05 20:15:18 +0700

    edit home admin

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -3,20 +3,20 @@
         ### PAGE START  
         ##########################################################################################
         -->
-        <div class="d-lg-none d-sm-none" style="
+<div class="d-lg-none d-sm-none" style="
     background: #0B1460;
     width: 100%;
-    height: 25rem;
+    height: 18rem;
     position: absolute;
     left: 0;
     top: 0;
-    border-radius: 43px;
+    border-radius: 15px;
 "></div>
-<div class="d-lg-none d-block px-3 ">
-    <div class="card bg-primary p-3">
-        <div class="media m-v-10 align-items-center">
+<div class="d-lg-none d-block px-2 ">
+    <div class="card bg-primary m-0 p-2 pt-4">
+        <div class="media  align-items-center">
             <div class="avatar avatar-text bg-white">
-                <span class="text-primary font-size-20"><?= substr($this->session->userdata("username"),0,1) ?></span>
+                <span class="text-primary font-size-20"><?= substr($this->session->userdata("username"), 0, 1) ?></span>
             </div>
             <div class="media-body p-l-15">
                 <h5 class="m-b-0 text-white"><?= $this->session->userdata("username") ?></h5>
@@ -30,7 +30,7 @@
         <div class="input-group">
             <input type="text" class="form-control form-control-sm bg-white " placeholder="Search leads..." aria-label="Search" aria-describedby="basic-addon2" name="search">
             <div class="input-group-append">
-                <button class="btn btn-primary " type="submit">
+                <button class="btn btn-light " type="submit">
                     <i class="fas fa-search fa-sm"></i>
                 </button>
             </div>
@@ -43,19 +43,21 @@
     <div class="row">
         <div class="col-lg-3 col-md-6 col-6 px-1">
             <a href="<?= base_url("admin/leads/new") ?>">
-                <div class="card w-100 align-items-stretch">
-                    <div class="card-body p-lg-4 p-md-4 p-sm-4 text-center">
+                <div class="card w-100 align-items-stretch mb-2">
+                    <div class="card-body p-lg-4 p-md-4 p-sm-4 p-3 text-center">
+                        <!-- icon mobile
                         <div class="avatar avatar-icon avatar-lg avatar-green d-lg-none d-md-none d-sm-none d-inline-block  mb-1">
                             <i class="anticon anticon-solution"></i>
                         </div>
-                        <div class="d-flex justify-content-lg-between justify-content-md-between justify-content-sm-between justify-content-center">
+                        -->
+                        <div class="d-flex justify-content-between">
                             <div>
                                 <p class="m-b-0 font-size-lg-14 font-size-10">New</p>
-                                <h2 class="m-b-0 ">
-                                    <span class="d-flex justify-content-lg-start justify-content-sm-start justify-content-center counter fw-bold" id="data-ds-new">0</span>
-                                </h2>
+                                <h5 class="m-b-0 ">
+                                    <span class="d-flex justify-content-start counter fw-bold" id="data-ds-new">0</span>
+                                </h5>
                             </div>
-                            <div class="avatar avatar-icon avatar-lg avatar-green d-lg-block d-md-block d-sm-block d-none">
+                            <div class="avatar avatar-icon avatar-lg avatar-green "> <!-- sebelumnya d-lg-block d-md-block d-sm-block d-none -->
                                 <i class="anticon anticon-solution"></i>
                             </div>
                         </div>
@@ -66,19 +68,21 @@
 
         <div class="col-lg-3 col-md-6 col-6 px-1 pb-0">
             <a href="<?= base_url("admin/leads/contacted") ?>">
-                <div class="card ">
-                    <div class="card-body p-lg-4 p-md-4 p-sm-4  text-center">
+                <div class="card mb-2">
+                    <div class="card-body p-lg-4 p-md-4 p-sm-4 p-3 text-center">
+                        <!--
                         <div class="avatar avatar-icon avatar-lg avatar-cyan d-lg-none d-md-none d-sm-none d-inline-block mb-1">
                             <i class="anticon anticon-message"></i>
                         </div>
-                        <div class="d-flex justify-content-lg-between justify-content-md-between justify-content-sm-between justify-content-center">
+                        -->
+                        <div class="d-flex justify-content-between">
                             <div>
                                 <p class="m-b-0 font-size-10">Contacted</p>
-                                <h2 class="m-b-0">
-                                    <span class="d-flex justify-content-lg-start justify-content-sm-start justify-content-center counter fw-bold"  id="data-ds-contacted">0</span>
-                                </h2>
+                                <h5 class="m-b-0">
+                                    <span class="d-flex justify-content-lg-start justify-content-start counter fw-bold" id="data-ds-contacted">0</span>
+                                </h5>
                             </div>
-                            <div class="avatar avatar-icon avatar-lg avatar-cyan d-lg-block d-md-block d-sm-block d-none">
+                            <div class="avatar avatar-icon avatar-lg avatar-cyan">
                                 <i class="anticon anticon-message"></i>
                             </div>
 
@@ -91,18 +95,20 @@
         <div class="col-lg-3 col-md-6 col-6 px-1 pb-0">
             <a href="<?= base_url("admin/leads/visit") ?>">
                 <div class="card">
-                    <div class="card-body p-lg-4 p-md-4 p-sm-4  text-center">
+                    <div class="card-body p-lg-4 p-md-4 p-sm-4 p-3 text-center">
+                        <!--
                         <div class="avatar avatar-icon avatar-lg avatar-gold d-lg-none d-md-none d-sm-none d-inline-block mb-1">
                             <i class="anticon anticon-calendar"></i>
                         </div>
-                        <div class="d-flex justify-content-lg-between justify-content-md-between justify-content-sm-between justify-content-center">
-                            <div class="justify-content-lg-start justify-content-md-start justify-content-sm-start  justify-content-center">
+                        -->
+                        <div class="d-flex justify-content-between">
+                            <div class="justify-content-start">
                                 <p class="m-b-0 font-size-10">Visit</p>
-                                <h2 class="m-b-0">
-                                    <span class="d-flex justify-content-lg-start justify-content-sm-start justify-content-center counter fw-bold"  id="data-ds-visit">0</span>
-                                </h2>
+                                <h5 class="m-b-0">
+                                    <span class="d-flex justify-content-start counter fw-bold" id="data-ds-visit">0</span>
+                                </h5>
                             </div>
-                            <div class="avatar avatar-icon avatar-lg avatar-gold d-lg-block d-md-block d-sm-block d-none">
+                            <div class="avatar avatar-icon avatar-lg avatar-gold">
                                 <i class="anticon anticon-calendar"></i>
                             </div>
                         </div>
@@ -113,25 +119,27 @@
         <div class="col-lg-3 col-md-6 col-6 px-1 pb-0 justify-content-center ">
             <a href="<?= base_url("admin/leads/close") ?>">
                 <div class="card ">
-                    <div class="card-body p-lg-4 p-md-4 p-sm-4 text-center">
+                    <div class="card-body p-lg-4 p-md-4 p-sm-4 p-3 text-center">
+                        <!--
                         <div class="avatar avatar-icon avatar-lg avatar-red d-lg-none d-md-none d-sm-none d-inline-block mb-1">
                             <i class="anticon anticon-star "></i>
                         </div>
-                        <div class="d-flex justify-content-lg-between justify-content-md-between justify-content-sm-between justify-content-center ">
-                            <div class="justify-content-lg-start justify-content-md-start justify-content-sm-start  justify-content-center">
+                        -->
+                        <div class="d-flex justify-content-between ">
+                            <div class="justify-content-start">
                                 <p class="m-b-0 font-size-10">Close</p>
-                                <h2 class="m-b-0 ">
-                                    <span class="d-flex justify-content-lg-start justify-content-center counter fw-bold" id="data-ds-close">0</span>
-                                </h2>
+                                <h5 class="m-b-0 ">
+                                    <span class="d-flex justify-content-start counter fw-bold" id="data-ds-close">0</span>
+                                </h5>
                             </div>
-                            <div class="avatar avatar-icon avatar-lg avatar-red d-lg-block d-md-block d-sm-block d-none">
+                            <div class="avatar avatar-icon avatar-lg avatar-red ">
                                 <i class="anticon anticon-star"></i>
                             </div>
                         </div>
                     </div>
                 </div>
             </a>
-        </div> 
+        </div>
     </div>
 </div>
 <!--  CARD DETAIL SCRIPT   -->
@@ -139,36 +147,36 @@
     $.ajax({
         dataType: "json",
         url: "<?= base_url("admin/get_card_dashboard") ?>",
-        success: function(data){ 
+        success: function(data) {
             //membuat delay
-            setTimeout(function() { 
-                jQuery.each(data, function(index, item) { 
+            setTimeout(function() {
+                jQuery.each(data, function(index, item) {
                     $("#" + index).text(item);
                 });
-                $('.counter').each(function () {
-                    $(this).prop('Counter',0).animate({
+                $('.counter').each(function() {
+                    $(this).prop('Counter', 0).animate({
                         Counter: $(this).text()
                     }, {
                         duration: 500,
                         easing: 'swing',
-                        step: function (now) {
+                        step: function(now) {
                             $(this).text(Math.ceil(now));
                         }
                     });
-                });  //your code to be executed after 1 second
+                }); //your code to be executed after 1 second
             }, 500);
         }
-    });   
+    });
 </script>
- 
-<div class="d-lg-flex d-block">
-    <div class="col-lg-8 col-12 d-flex align-items-stretch ">
+
+<div class="d-lg-flex d-block ">
+    <div class="col-lg-8 col-12 d-flex align-items-stretch p-0">
         <div class="card w-100">
             <div class="card-body">
                 <div class="d-flex justify-content-between align-items-center">
                     <h5>Recent New</h5>
                     <div>
-                        <a href="<?= base_url("admin/leads")?>" class="btn btn-sm btn-primary">View All</a>
+                        <a href="<?= base_url("admin/leads") ?>" class="btn btn-sm btn-primary">View All</a>
                     </div>
                 </div>
                 <div class="m-t-30">
@@ -199,12 +207,12 @@
                                         <div class="font-size-13 align-items-center d-lg-block d-md-block d-none col">
                                             <h6>Source</h6>
                                         </div>
-                                    </div> 
-                                </li>  
+                                    </div>
+                                </li>
                             </ul>
-                            
+
                             <div class="text-center loading-new">
-                                <img src = "<?= base_url("assets/images/others/Spin-loading.svg") ?>" alt="My Happy SVG"/>
+                                <img src="<?= base_url("assets/images/others/Spin-loading.svg") ?>" alt="My Happy SVG" />
                             </div>
                         </div>
                     </div>
@@ -218,7 +226,7 @@
                 <div class="d-flex justify-content-between align-items-center">
                     <h5>Recent Deals</h5>
                     <div>
-                        <a href="<?= base_url("admin/leads")?>" class="btn btn-sm btn-primary">View All</a>
+                        <a href="<?= base_url("admin/leads") ?>" class="btn btn-sm btn-primary">View All</a>
                     </div>
                 </div>
                 <div class="m-t-30">
@@ -237,12 +245,12 @@
                                         <div class="font-size-13 align-items-center col">
                                             <h6>Category</h6>
                                         </div>
-                                    </div> 
-                                </li>  
+                                    </div>
+                                </li>
                             </ul>
-                            
+
                             <div class="text-center loading-deals">
-                                <img src = "<?= base_url("assets/images/others/Spin-loading.svg") ?>" alt="My Happy SVG"/>
+                                <img src="<?= base_url("assets/images/others/Spin-loading.svg") ?>" alt="My Happy SVG" />
                             </div>
                         </div>
                     </div>
@@ -257,10 +265,10 @@
     $.ajax({
         dataType: "json",
         url: "<?= base_url("admin/get_list_new_dashboard") ?>",
-        success: function(data){   
-            jQuery.each(data, function(index, item) {  
-                var date_new = moment(item["date_new"] + " " +  item["time_new"] );  
-                switch(item["category"]){
+        success: function(data) {
+            jQuery.each(data, function(index, item) {
+                var date_new = moment(item["date_new"] + " " + item["time_new"]);
+                switch (item["category"]) {
                     case 'New':
                         var color = "cyan";
                         break;
@@ -293,10 +301,10 @@
                         var color = "gold";
                         break;
                     default:
-                        var color = "green"; 
-                }  
+                        var color = "green";
+                }
                 var html = `
-                    <li class="list-group-item p-h-0 align-items-center list new" onclick="location.href='<?= base_url("admin/data_leads/")?>${item['id']}'" style="display:none">
+                    <li class="list-group-item p-h-0 align-items-center list new" onclick="location.href='<?= base_url("admin/data_leads/") ?>${item['id']}'" style="display:none">
                         <div class="d-flex align-items-center justify-content-between">
                             <div class="d-flex align-items-center col-lg-3 col-md-5 col-0 pl-lg pl-0">
                                 <div class="avatar avatar-text bg-default m-r-15">
@@ -329,21 +337,21 @@
                             </div>
                         </div>
                     </li>`;
-                $("#list-recent-new").append(html); 
-                $(".list.new").delay(index * 200).fadeIn();   
-            }); 
-            $(".loading-new").fadeOut(); 
+                $("#list-recent-new").append(html);
+                $(".list.new").delay(index * 200).fadeIn();
+            });
+            $(".loading-new").fadeOut();
         }
-    });   
+    });
 
-    
+
     $.ajax({
         dataType: "json",
         url: "<?= base_url("admin/get_list_deal_dashboard") ?>",
-        success: function(data){   
-            jQuery.each(data, function(index, item) {  
-                var date_new = moment(item["date_new"] + " " +  item["time_new"] );  
-                switch(item["category"]){
+        success: function(data) {
+            jQuery.each(data, function(index, item) {
+                var date_new = moment(item["date_new"] + " " + item["time_new"]);
+                switch (item["category"]) {
                     case 'New':
                         var color = "cyan";
                         break;
@@ -376,10 +384,10 @@
                         var color = "gold";
                         break;
                     default:
-                        var color = "green"; 
-                }  
+                        var color = "green";
+                }
                 var html = `
-                    <li class="list-group-item p-h-0 align-items-center list deals" onclick="location.href='<?= base_url("admin/data_leads/")?>${item['id']}'" style="display:none">
+                    <li class="list-group-item p-h-0 align-items-center list deals" onclick="location.href='<?= base_url("admin/data_leads/") ?>${item['id']}'" style="display:none">
                         <div class="d-flex align-items-center justify-content-between">
                             <div class="d-flex align-items-center pl-lg pl-0 col-lg-9 col-md-10 col-sm-10 col-0">
                                 <div class="avatar avatar-text bg-default m-r-15">
@@ -400,12 +408,12 @@
                             </div> 
                         </div>
                     </li>`;
-                $("#list-recent-deals").append(html); 
-                $(".list.deals").delay(index * 200).fadeIn();   
-            }); 
-            $(".loading-deals").fadeOut(); 
+                $("#list-recent-deals").append(html);
+                $(".list.deals").delay(index * 200).fadeIn();
+            });
+            $(".loading-deals").fadeOut();
         }
-    });   
+    });
 </script>
 
 <!--
